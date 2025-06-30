2c74bca00ccb725b2f9351a2520a641be78fc778
NAYAN-KAHAR <kaharnayan99@gmail.com>
2025-06-30 19:23:42 +0530

    app completed

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import helmet from 'helmet';
+import mongoose from "./config/db.js";
+import quizRoute from './Routes/quizRoute.js';
+import authRoute from './Routes/authRoute.js';
+import cookieParser from 'cookie-parser';
+
+
+// Initialize the Express app
+const app = express();
+const port = 3000;
+
+// Middleware
+// This is the CORS middleware, which helps manage Cross-Origin Resource Sharing — a security feature enforced by browsers that blocks requests from different origins unless explicitly allowed.
+
+app.use(cors({
+  origin: 'https://keen-lily-0f0c45.netlify.app',
+  credentials: true,
+}));
+app.options('*', cors()); // ✅ allow preflight requests
+
+// This allows the frontend to send cookies or authorization headers with cross-origin requests.
+app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
+
+mongoose.connection.on('open', () => {
+  console.log('Database Connected')
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Connection Failed', err);
+})
+
+app.use('/api', quizRoute);
+app.use('/api', authRoute);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
---
+++ client/src/Components/Authentication/protectedRoute.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { ModalContext } from '../context/userContext';
+
+const ProtectedRoute = ({ children }) => {
+  const [auth, setAuth] = useState(null);
+  const modal = useContext(ModalContext);
+  const quizeUser = localStorage?.getItem('quizeUser')?.split(' ')[0];
+  
+    const url = import.meta.env.VITE_BACKEND_URL;
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await axios.get(`${url}/api/verify`, {withCredentials: true });
+        const authenticate = res?.data?.authenticated;
+        setAuth(authenticate);
+        // console.log('res.data.authenticated:', authenticate);
+      } catch (err) {
+        // console.error('Auth check failed:', err);
+        setAuth(false); // Explicitly mark as unauthenticated
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    // console.log('auth updated:', auth);
+    if (auth === false) {
+      modal.setIsModal(true); // Show a modal saying "you must be logged in"
+    }
+  }, [auth]);
+
+  if (auth === null) return <div>Loading...</div>; //If still checking auth (null): show Loading...
+  if (auth === false) return <Navigate to="/" replace />; // replace -> Redirect, but don’t keep the current page in history
+  return children
+  
+};
+
+export default ProtectedRoute;
